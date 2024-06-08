d40abf751b3a64e48bf872d496da96c64178ddd6
RahulDawpersad <135977072+RahulDawpersad@users.noreply.github.com>
2024-06-08 23:39:04 +0200

    third commit

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello Developer✌</h1>"
+    return "<h1>Hello developers, welcome to DesignX!</h1>"
 
-# This block ensures that the Flask app is started correctly
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
